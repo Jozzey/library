e151dbd02c5ee23884408eccd6dcb34f983eefb3
Jozzey <jozzey@hotmail.com>
2019-01-07 11:12:52 +0000

    Finished setting up bootstrap & added jquery

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ var path = require("path");
 var app = express();
 
 app.use(morgan("tiny"));
+app.use(express.static(path.join(__dirname, "/public/")));
+// Below are locations other than public that hold css & js
+app.use("/css", express.static(path.join(__dirname,"/node_modules/bootstrap/dist/css")));
+app.use("/js", express.static(path.join(__dirname,"/node_modules/bootstrap/dist/js")));
+app.use("/js", express.static(path.join(__dirname,"/node_modules/jquery/dist")));
 
 app.get("/", function(req, res){
     res.sendFile(path.join(__dirname, "views/index.html"));
