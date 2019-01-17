79dc8d211482add723977ffcb6c15d5be3538c29
Jozzey <jozzey@hotmail.com>
2019-01-17 14:26:33 +0000

    Added authentication using passport

--- app.js
+++ app.js
@@ -3,26 +3,23 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
-const sql = require('mssql');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// mssql config
-const config = {
-  user: 'library',
-  password: 'Ch0rt1e$',
-  server: 'jayslibrary.database.windows.net', // You can use 'localhost\\instance' to connect to named instance
-  database: 'PSlibrary',
-
-  options: {
-    encrypt: true // Use this if you're on Windows Azure
-  }
-};
-
-sql.connect(config).catch(err => debug(err));
 
 app.use(morgan('tiny'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(session({ secret: 'library' }));
+
+require('./src/config/passport.js')(app);
+
 app.use(express.static(path.join(__dirname, '/public/')));
 // Below are locations other than public that hold css & js
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
@@ -40,9 +37,11 @@ const nav = [
 
 const bookRouter = require('./src/routes/bookRoutes')(nav);
 const adminRouter = require('./src/routes/adminRoutes')(nav);
+const authRouter = require('./src/routes/authRoutes')(nav);
 
 app.use('/books', bookRouter);
 app.use('/admin', adminRouter);
+app.use('/auth', authRouter);
 
 app.get('/', (req, res) => {
   res.render(
